1f5a0b2a124826f44f94c0cc32c6e4d30a5f45c3
Clarence <mr.lin.clarence@gmail.com>
2015-04-29 13:53:50 +0800

    Update BCNM HW 03 - pktcapture device options

--- BCNM/HW03/pktcapture/main.cpp
+++ BCNM/HW03/pktcapture/main.cpp
@@ -14,48 +14,81 @@ void my_callback(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char*
 
 int main(int argc,char **argv)
 {
-	pcap_if_t *devlist;
-    char *dev;
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* descr;
-    struct bpf_program fp;      /* hold compiled program     */
-    bpf_u_int32 maskp;          /* subnet mask               */
-    bpf_u_int32 netp;           /* ip                        */
-    u_char* args = NULL;
+  pcap_if_t *devlist, *device;
+  char *dev, *filter;
+  char errbuf[PCAP_ERRBUF_SIZE];
+  pcap_t* descr;
+  struct bpf_program fp;      /* hold compiled program     */
+  bpf_u_int32 maskp;          /* subnet mask               */
+  bpf_u_int32 netp;           /* ip                        */
+  u_char* args = NULL;
+  int ch, npackets;
 
-    if(argc < 2){
-        fprintf(stdout,"Usage: %s numpackets\n",argv[0]);
-        return 0;
-    }
+  dev = filter = NULL;
 
-    /* grab a device to peak into... */
-    dev = pcap_lookupdev(errbuf);
-    if(dev == NULL)
-    { printf("%s\n",errbuf); exit(1); }
+	while((ch = getopt(argc, argv, "f:i:l")) != -1)	{
+		switch(ch){
+		case 'f':
+			filter = optarg;
+			break;
+		case 'i':
+			dev = optarg;
+			break;
+		case 'l':
+		    /* get the devices list*/
+		    if(pcap_findalldevs(&devlist, errbuf) == -1){
+		    	printf("%s\n", errbuf);
+		    	exit(1);
+		    }
+		    /* list the device*/
+		    for(device = devlist; device; device = device->next){
+		    	printf("%s - %s\n", device->name, device->description);
+		    }
+		    pcap_freealldevs(devlist);
+			break;
+		default:
+			fprintf(stdout,"Usage: %s numpackets\n",argv[0]);
+			exit(1);
+		}
+	}
 
-    /* ask pcap for the network address and mask of the device */
-    pcap_lookupnet(dev,&netp,&maskp,errbuf);
+	if(argc - optind != 1)
+	{
+		fprintf(stdout,"Usage: %s numpackets\n",argv[0]);
+		exit(1);
+	}
+	npackets = atoi(argv[optind]);
 
-    /* open device for reading. NOTE: defaulting to
-     * promiscuous mode*/
-    descr = pcap_open_live(dev,BUFSIZ,1,-1,errbuf);
-    if(descr == NULL)
-    { printf("pcap_open_live(): %s\n",errbuf); exit(1); }
+  if(dev == NULL)
+  {
+		dev = pcap_lookupdev(errbuf);
+		if(dev == NULL)
+		{ printf("%s\n",errbuf); exit(1); }
+  }
 
+  /* ask pcap for the network address and mask of the device */
+  pcap_lookupnet(dev,&netp,&maskp,errbuf);
 
-    if(argc > 2)
-    {
-      /* Lets try and compile the program.. non-optimized*/
-      if(pcap_compile(descr, &fp, argv[2], 0, netp) == -1)
-      { fprintf(stderr, "Error calling pcap_compile.\n"); exit(1); }
+  /* open device for reading. NOTE: defaulting to
+   * promiscuous mode*/
+  descr = pcap_open_live(dev,BUFSIZ,1,-1,errbuf);
+  if(descr == NULL)
+  { printf("pcap_open_live(): %s\n",errbuf); exit(1); }
 
-      /* set the compiled program as the filter */
-      if(pcap_setfilter(descr, &fp) == -1)
-      { fprintf(stderr, "Error setting filter\n"); exit(1); }
-    }
+  if(filter != NULL)
+  {
+  	/* Lets try and compile the program.. non-optimized*/
+    if(pcap_compile(descr, &fp, filter, 0, netp) == -1)
+    { fprintf(stderr, "Error calling pcap_compile.\n"); exit(1); }
 
-    pcap_loop(descr,atoi(argv[1]),my_callback,args);
+    /* set the compiled program as the filter */
+    if(pcap_setfilter(descr, &fp) == -1)
+    { fprintf(stderr, "Error setting filter\n"); exit(1); }
+  }
 
-    fprintf(stdout,"\nfinished\n");
-    return 0;
+  pcap_loop(descr, npackets, my_callback,args);
+
+  fprintf(stdout,"\nfinished\n");
+
+	return 0;
 }
